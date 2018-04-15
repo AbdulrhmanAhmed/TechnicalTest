0f8cb873d4436971adcfb07989a7a1ce0cf5dcda
AbdulrhmanAhmed <38407560+AbdulrhmanAhmed@users.noreply.github.com>
2018-04-15 22:43:43 +0200

    Add files via upload

---
+++ Task_Tracking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TasskTracking.Models
+{
+    public class Task_Tracking
+    {
+        public int id  { get; set; }
+        [Required]
+        [Display(Name="Task Title")]
+        public string task_Title { get; set; }
+        [Required]
+        [Display(Name = "Task Describtion")]
+        public string Task_Description { get; set; }
+        [Required]
+        [Display(Name = "Assingee")]
+        public string Assingee { get; set; }
+        [Required]
+        [Display(Name = "Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime due_date { get; set; }
+        [Required]
+        [Display(Name = "Priority")]
+        public string priority { get; set; }
+        [Required]
+        [Display(Name = "Statue")]
+        public string statue { get; set; }
+    }
+}
---
+++ TasksTrackingController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasskTracking.Models;
+
+namespace TasskTracking.Controllers
+{
+    public class TasksTrackingController : Controller
+    {
+        //
+        ApplicationDbContext dn = new ApplicationDbContext();
+        // GET: /TasksTracking/
+        public ActionResult Index()
+        {
+            return View(dn.Task_Tracking.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.priority = new SelectList(new[] { "High", "Medium", "Low" });
+            ViewBag.statue = new SelectList(new[] { "Open", "Deferred", "Fixed", "Closed" });
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Task_Tracking task)
+        {
+          if(ModelState.IsValid){
+
+              dn.Task_Tracking.Add(task);
+              dn.SaveChanges();
+              return RedirectToAction("Index");
+          }
+
+            return View(task);
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+            Task_Tracking task = dn.Task_Tracking.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            
+            }
+            return View(task);
+        }
+
+	}
+}
